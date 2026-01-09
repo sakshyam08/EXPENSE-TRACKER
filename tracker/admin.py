571f35ad6e985222149bf93af574df68cb61dc61
sakshyam08 <sakshyamkoirala77@.com>
2026-01-09 07:45:03 +0545

    adding features in tracker

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from tracker.models import *
 # Register your models here.
+admin.site.site_header = "Expense Tracker Admin"
+admin.site.site_title = "Expense Tracker Admin Portal"
+
+admin.site.register(CurrentBalance)
+
+class TrackingHistoryAdmin(admin.ModelAdmin):
+    list_display = ('date', 'amount', 'description','expense_type')
+
+admin.site.register(TrackingHistory, TrackingHistoryAdmin)
